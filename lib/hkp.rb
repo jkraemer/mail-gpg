6eb2dc5a41f983c7e44b644c42956237c39094ae
Jens Kraemer <jk@jkraemer.net>
2013-08-27 00:57:46 +0200

    document and enhance the hkp client
    - also bump to 0.0.5

--- lib/hkp.rb
+++ lib/hkp.rb
@@ -1,26 +1,46 @@
 require 'open-uri'
+require 'gpgme'
+
+# simple HKP client for public key retrieval
 class Hkp
   def initialize(keyserver = 'http://pool.sks-keyservers.net:11371')
     @keyserver = keyserver
   end
 
+  # hkp.search 'user@host.com'
+  # will return an array of arrays, one for each matching key found, containing
+  # the key id as the first elment and any further info returned by the key
+  # server in the following elements.
+  # see http://tools.ietf.org/html/draft-shaw-openpgp-hkp-00#section-5.2 for
+  # what that *might* be. unfortunately key servers seem to differ in how much
+  # and what info they return besides the key id
   def search(name)
     [].tap do |results|
       open("#{@keyserver}/pks/lookup?options=mr&search=#{URI.escape name}") do |f|
         f.each_line do |l|
-          if l =~ /pub:(\w{8}):/
-            results << $1
+          components = l.strip.split(':')
+          if components.shift == 'pub'
+            results << components
           end
         end
       end
     end
   end
 
+  # returns the key data as returned from the server as a string
   def fetch(id)
     open("#{@keyserver}/pks/lookup?options=mr&op=get&search=0x#{URI.escape id}") do |f|
       return clean_key f.read
     end
+  end
 
+  # fetches key data by id and imports the found key(s) into GPG, returning the full hex fingerprints of the
+  # imported key(s) as an array. Given there are no collisions with the id given / the server has returned
+  # exactly one key this will be a one element array.
+  def fetch_and_import(id)
+    if key = fetch(id)
+      GPGME::Key.import(key).imports.map(&:fpr)
+    end
   end
 
   private
