639657800f93686ae8a35f808f6930b873ac3011
Morten Andersen <github@accel.dk>
2014-02-16 14:26:35 +0100

    added signature check for RFC3156 signed mails

--- lib/mail/gpg/sign_part.rb
+++ lib/mail/gpg/sign_part.rb
@@ -11,6 +11,14 @@ module Mail
           content_description 'OpenPGP digital signature'
         end
       end
+
+      def self.signature_valid?(plain, signature, options = {})
+        if !(signature.has_content_type? && ('application/pgp-signature' == signature.mime_type))
+          return false
+        end
+
+        GpgmeHelper.sign_verify(plain.encoded, signature.body.encoded, options)
+      end
     end
   end
 end
