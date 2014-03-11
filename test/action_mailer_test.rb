13758dda2f156f33a15359ff106404feb4c24483
Dominik Menke <dom@digineo.de>
2014-03-11 15:00:32 +0100

    cleanup code: use consistend indentation

--- test/action_mailer_test.rb
+++ test/action_mailer_test.rb
@@ -11,17 +11,16 @@ class MyMailer < ActionMailer::Base
     mail subject: 'encrypted', body: 'encrypted mail', gpg: {encrypt: true}
   end
 
-	def signed
-		mail	from: 'jane@foo.bar',
-					to: 'joe@foo.bar',
-					subject: 'signed', 
-					body: 'signed mail', 
-					gpg: {
-						sign: true,
-						password: 'abc'
-					}
-	end
-
+  def signed
+    mail  from: 'jane@foo.bar',
+          to: 'joe@foo.bar',
+          subject: 'signed',
+          body: 'signed mail',
+          gpg: {
+            sign: true,
+            password: 'abc'
+          }
+  end
 end
 
 class ActionMailerTest < Test::Unit::TestCase
@@ -52,19 +51,19 @@ class ActionMailerTest < Test::Unit::TestCase
       assert_equal 'encrypted mail', m.body.to_s
     end
 
-		should "send signed mail" do
-			assert m = MyMailer.signed
-			assert true == m.gpg[:sign]
-			m.deliver
-			assert_equal 1, @emails.size
-			assert delivered = @emails.first
-			assert_equal 'signed', delivered.subject
-			assert_equal 2, delivered.parts.size
-			assert sign_part = delivered.parts.detect{|p| p.content_type =~ /signature\.asc/}
-			GPGME::Crypto.new.verify(sign_part.body.to_s, signed_text: m.encoded) do |sig| 
-				assert true == sig.valid?
-			end
-		end
+    should "send signed mail" do
+      assert m = MyMailer.signed
+      assert true == m.gpg[:sign]
+      m.deliver
+      assert_equal 1, @emails.size
+      assert delivered = @emails.first
+      assert_equal 'signed', delivered.subject
+      assert_equal 2, delivered.parts.size
+      assert sign_part = delivered.parts.detect{|p| p.content_type =~ /signature\.asc/}
+      GPGME::Crypto.new.verify(sign_part.body.to_s, signed_text: m.encoded) do |sig|
+        assert true == sig.valid?
+      end
+    end
 
   end
 end
--- test/sign_part_test.rb
+++ test/sign_part_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 require 'mail/gpg/sign_part'
 
 class SignPartTest < Test::Unit::TestCase
-	context 'SignPart' do
-		setup do
-			@mail = Mail.new do
-				to 'jane@foo.bar'
-				from 'joe@foo.bar'
-				subject 'test'
-				body 'i am unsigned'
-			end
-		end
+  context 'SignPart' do
+    setup do
+      @mail = Mail.new do
+        to 'jane@foo.bar'
+        from 'joe@foo.bar'
+        subject 'test'
+        body 'i am unsigned'
+      end
+    end
 
-		should 'roundtrip successfully' do
-			signature_part = Mail::Gpg::SignPart.new(@mail, password: 'abc')
-			assert Mail::Gpg::SignPart.signature_valid?(@mail, signature_part)
-		end
-	end
+    should 'roundtrip successfully' do
+      signature_part = Mail::Gpg::SignPart.new(@mail, password: 'abc')
+      assert Mail::Gpg::SignPart.signature_valid?(@mail, signature_part)
+    end
+  end
 end
