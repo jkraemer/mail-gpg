e1095a718f5f27729d8cb542fe228a18006db203
Jens Kraemer <jk@jkraemer.net>
2016-09-28 13:21:32 +0800

    get rid of alias_method_chain

--- lib/mail/gpg/rails.rb
+++ lib/mail/gpg/rails.rb
@@ -2,6 +2,8 @@ begin
   require 'action_mailer'
   require 'active_support'
   require 'mail/gpg/rails/action_mailer_base_patch'
+
+  Mail::Gpg::Rails::ActionMailerPatch.apply
 rescue LoadError
   # no actionmailer, do nothing
 end
--- lib/mail/gpg/rails/action_mailer_base_patch.rb
+++ lib/mail/gpg/rails/action_mailer_base_patch.rb
@@ -5,37 +5,34 @@ module Mail
     module Rails
 
       module ActionMailerPatch
-        extend ActiveSupport::Concern
 
-        included do
-          alias_method_chain :mail, :gpg
-          class << self
-            alias_method_chain :deliver_mail, :gpg
+        def self.apply
+          unless ActionMailer::Base < InstanceMethods
+            ActionMailer::Base.prepend InstanceMethods
+            ActionMailer::Base.singleton_class.prepend ClassMethods
           end
         end
 
-        def mail_with_gpg(headers = {}, &block)
-          headers = headers.dup
-          gpg_options = headers.delete :gpg
-          mail_without_gpg(headers, &block).tap do |m|
-            if gpg_options
-              m.gpg gpg_options
+        module InstanceMethods
+          def mail(headers = {}, &block)
+            headers = headers.dup
+            gpg_options = headers.delete :gpg
+            super(headers, &block).tap do |m|
+              if gpg_options
+                m.gpg gpg_options
+              end
             end
           end
         end
 
         module ClassMethods
-          def deliver_mail_with_gpg(mail, &block)
-            deliver_mail_without_gpg(mail) do
+          def deliver_mail(mail, &block)
+            super(mail) do
               Mail::Gpg::DeliveryHandler.deliver_mail mail, &block
             end
           end
         end
 
-      end
-
-      unless ActionMailer::Base.included_modules.include?(ActionMailerPatch)
-        ActionMailer::Base.send :include, ActionMailerPatch
       end
 
     end
